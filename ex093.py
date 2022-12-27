bb2ec92d595e490f870e17801de8731976c3d7b1
Deivid7x <115668940+Deivid7x@users.noreply.github.com>
2022-12-26 21:28:25 -0300

    Ajustes na experiência do usuário no ex093.

--- ex093.py
+++ ex093.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 registro = {'nome': input('Digite o nome: ')}
 partidas = int(input(f'Quantas partidas {registro["nome"]} jogou? '))
 
@@ -12,11 +14,22 @@ for c in range(0, partidas):
 registro['gols'] = listag.copy()
 registro['total'] = totalgols
 
+print('Calculando...')
+sleep(2)
+
 print(f'Nome: {registro["nome"]}\n'
       f'Gols em cada partida: {registro["gols"]}\n'
       f'Ele marcou no total: {registro["total"]} Gols.')
 print('=-'*20)
+
+print('Preparando dados detalhados...')
+sleep(3)
+
 print(f'O jogador {registro["nome"]} jogou {partidas} partidas.')
 for c in range(0, partidas):
     print(f'   => Na partida {c+1}, ele fez {registro["gols"][c]}')
-print(f'Foi um total de {registro["total"]}')
+print(f'Foi um total de {registro["total"]} gols.\n')
+
+sleep(2)
+
+print('Programa encerrado.')
